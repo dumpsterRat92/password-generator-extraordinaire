5277c9c7573f623dc4910e01b3756cd443d2d3da
Your Name <you@example.com>
2024-02-20 19:22:49 -0700

    wrote js for password random generator prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,65 @@
 // Assignment code here
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
 
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//   passwordText.value = password;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// }
 
-  passwordText.value = password;
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
 
-}
+// function generatePassword() {
+// var minNum = 8
+// var maxNum = 128
+// var lettersBig = ['A', 'B','C','D', 'E', 'F', 'G', 'H', 'I', 'K', 'L' ,'M', 'N', 'O','P','Q', 'R','S','T','U','V','W','X','Y','Z'];
+// var letterSmall = ['a', 'b', 'c', 'd','e', 'f','g','h','i','j','k','l','m','n','o','p','q','r','r','s','t','u','v','w','x','y','z'];
+// var passwordSize = NaN;
+// var numbers = math.random*10;
+// var passwordSize = NaN;
+// var useBig, useSmall, useChars, useNums = false;
+// var C
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// do {
+//    lettersBig = confirm("")
+// }
 
-function generatePassword() {
-var minNum = 8
-var maxNum = 128
-var lettersBig = ['A', 'B','C','D', 'E', 'F', 'G', 'H', 'I', 'K', 'L' ,'M', 'N', 'O','P','Q', 'R','S','T','U','V','W','X','Y','Z'];
-var letterSmall = ['a', 'b', 'c', 'd','e', 'f','g','h','i','j','k','l','m','n','o','p','q','r','r','s','t','u','v','w','x','y','z'];
-var passwordSize = NaN;
-var numbers = math.random*10;
-var passwordSize = NaN;
-var useBig, useSmall, useChars, useNums = false;
-var C
-}
+// const passwordBox = document.getElementById("password");
+// const length = 12;
 
-do {
-   lettersBig = confirm("")
-}
+// const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+// const number = "0123456789";
+// const symbol = "@#$%^&*()_-=+~><{}[]/";
+
+// const allChars = upperCase + lowerCase + number + symbol;
+
+// function createPassword(){
+//   let password = "";
+//   password += upperCase[Math.floor(Math.random() * upperCase.length)];
+//   password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+//   password += number[Math.floor(Math.random() * number.length)];
+//   password += symbol[Math.floor(Math.random() * symbol.length)];
+
+//   while(length > password.length){
+//     password += allChars[Math.floor(Math.random() * allChars.length)];
+//   }
+//   passwordBox.value = password;
+// }
+
+// const btnEl = document.querySelector(".btn")
+//  btnEl.addEventListener("click", ()=>{
+//   createPassword()
+//  })
+
+//  let password = ""
+//  for (let index = 0; index < length; index++) {
+//    const randomNum = Math.floor(Math.random() * allChars.length)
+//  }
+
